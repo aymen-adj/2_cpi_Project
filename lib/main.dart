fa3f9f356745365dd382b2a747968484cde4eacf
AymenADJ <adj.abdeldjalil@gmail.com>
2021-04-27 11:58:49 +0100

    wilaya_list_0.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -55,48 +55,44 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Container(
-        child: Column(
-          children: [
-            ReorderableListView(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              //
-              children: <Widget>[
-                for (int index = 0; index < _items.length; index++)
-                  DropdownButton(
-                    key: Key('$index'),
-                    hint: (index == 0) ? Text("الانطلاق") : Icon(Icons.add),
-                    iconSize: 0,
-                    value: _items[index],
-                    items: _dropDownMenuItems,
-                    focusColor: Colors.blue,
-                    onChanged: (String newValue) {
-                      setState(() {
-                        _items[index] = newValue;
-                      });
-                    },
-                  ),
-                ElevatedButton(
-                  key: ValueKey(58),
-                  child: Text('Add'),
-                  onPressed: () {
-                    setState(() {
-                      _items.insert(_items.length, null);
-                    });
-                  },
-                ),
-              ],
-              //
-              onReorder: (int oldIndex, int newIndex) {
+        child: ReorderableListView(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          //
+          children: <Widget>[
+            for (int index = 0; index < _items.length; index++)
+              DropdownButton(
+                key: Key('$index'),
+                hint: (index == 0) ? Text("الانطلاق") : Icon(Icons.add),
+                iconSize: 0,
+                value: _items[index],
+                items: _dropDownMenuItems,
+                focusColor: Colors.blue,
+                onChanged: (String newValue) {
+                  setState(() {
+                    _items[index] = newValue;
+                  });
+                },
+              ),
+            ElevatedButton(
+              key: ValueKey(58),
+              child: Text('Add'),
+              onPressed: () {
                 setState(() {
-                  if (oldIndex < newIndex) {
-                    newIndex -= 1;
-                  }
-                  final String item = _items.removeAt(oldIndex);
-                  _items.insert(newIndex, item);
+                  _items.insert(_items.length, null);
                 });
               },
             ),
           ],
+          //
+          onReorder: (int oldIndex, int newIndex) {
+            setState(() {
+              if (oldIndex < newIndex) {
+                newIndex -= 1;
+              }
+              final String item = _items.removeAt(oldIndex);
+              _items.insert(newIndex, item);
+            });
+          },
         ),
       ),
     );
