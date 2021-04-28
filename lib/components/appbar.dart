ce730c9930f703ad6af13b8e029caf5fbd1391ba
mosbah <mosbah.avion12345@gmail.com>
2021-04-28 01:02:06 +0100

    homepage

---
+++ lib/components/appbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  minRadius:30,
+                  maxRadius: 50,
+                ),
+                Text('mosbah'),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,126 @@
 import 'package:flutter/material.dart';
 import 'constantes/constants.dart';
+import 'package:ii_cpi_project/screens/profileScreen.dart';
+import 'components/appbar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/': (contex) => Home(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
+const kMyColor = Colors.red;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int selected = 0;
+  PageController index = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
+    return Scaffold(
+      drawer: MyDrawer(),   //   components/appbar.dart
+      appBar: AppBar(
 
-          body: Center(
-            child: TrajetBox(),
-          ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+                icon: Icon(
+                  Icons.home,
+                  color: selected == 0 ? kMyColor : Colors.grey,
+                ),
+                iconSize: 36,
+                onPressed: () {
+                  setState(() {
+                    selected = 0;
+                    index.jumpToPage(0);
+                  });
+                }),
+            IconButton(
+                icon: Icon(
+                  Icons.contacts_rounded,
+                  color: selected == 1 ? kMyColor : Colors.grey,
+                ),
+                iconSize: 36,
+                onPressed: () {
+                  setState(() {
+                    index.jumpToPage(1);
+                    selected = 1;
+                  });
+                }),
+            IconButton(
+                icon: Icon(
+                  Icons.schedule_send,
+                  color: selected == 2 ? kMyColor : Colors.grey,
+                ),
+                iconSize: 36,
+                onPressed: () {
+                  setState(() {
+                    index.jumpToPage(2);
+                    selected = 2;
+                  });
+                }),
+            IconButton(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: selected == 3 ? kMyColor : Colors.grey,
+                ),
+                iconSize: 36,
+                onPressed: () {
+                  setState(() {
+                    index.jumpToPage(3);
+                    selected = 3;
+                  });
+                }),
+          ],
         ),
+      ),
+      body: PageView(
+        onPageChanged: (ind) {
+          setState(() {
+            selected = ind;
+          });
+        },
+        controller: index,
+        children: [
+          HomePage(),
+          TrajetBox(),
+          ProfileScreen(),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.teal,
       ),
     );
   }
