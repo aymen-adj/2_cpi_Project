65f462e06064e92c0bfd83940ccc896c80d82642
karim <zaghlaouitite@gmail.com>
2021-05-21 14:54:15 +0100

    kk

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ii_cpi_project/screens/CreateDemande.dart';
 import 'package:ii_cpi_project/screens/CreateOffer.dart';
-import 'package:ii_cpi_project/screens/First.dart';
+import 'package:ii_cpi_project/screens/LogInUsingPhone.dart';
 import 'package:ii_cpi_project/screens/Profile.dart';
 import 'package:ii_cpi_project/screens/Sign%20up.dart';
 import 'package:ii_cpi_project/screens/log_in.dart';
@@ -15,7 +15,7 @@ void main() async {
       // theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => First(),
+        '/': (context) => LogInUsingPhone(),
         ProfileScreen.id: (context) => ProfileScreen(),
         CreateOffer.id: (context) => CreateOffer(),
         CreateDemande.id: (context) => CreateDemande(),
