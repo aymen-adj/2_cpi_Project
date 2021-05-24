86732a6ecf3686469ecaef5f444b9d93910dc972
AouinaneMoussa <64875102+AouinaneMoussa@users.noreply.github.com>
2021-05-24 17:58:46 +0100

    Rating Screen

---
+++ android/app/src/debug/gen/com/example/ii_cpi_project/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.ii_cpi_project;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
