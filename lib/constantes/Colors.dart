d9be903670172fe16dc3d35745d1def4ace89cb9
Cinex10 <63453776+Cinex10@users.noreply.github.com>
2021-06-02 01:02:56 +0200

    Colors constantes updated

--- lib/constantes/Colors.dart
+++ lib/constantes/Colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-const ActiveColor = Colors.blueGrey;
+const kColor1 = Color(0xFF003049);
+const kColor2 = Color(0xFFD62828);
+const kColor3 = Color(0xFFF77F00);
+const kColor4 = Color(0xFFFCBF49);
+const kColor5 = Color(0xFFEAE2B7);
